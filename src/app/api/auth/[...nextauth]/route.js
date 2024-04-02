0415e37c2e16a3f1ccbf10d40011b604fb20ebeb
almog-cohe <almogqwerty@gmail.com>
2024-04-02 23:37:03 +0300

    Fixing file before deploying

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -7,45 +7,7 @@ import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "@/libs/mongoConnect.js";
 import { UserInfo } from "@/app/models/UserInfo.js";
-
-export const authOptions = {
-  session: {
-    strategy: "jwt",
-  },
-  secret: process.env.SECRET,
-  adapter: MongoDBAdapter(clientPromise),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    CredentialsProvider({
-      name: "Credentials",
-      id: "credentials",
-      credentials: {
-        username: {
-          label: "Email",
-          type: "email",
-          placeholder: "test@example.com",
-        },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        const email = credentials?.email;
-        const password = credentials?.password;
-
-        mongoose.connect(process.env.MONGO_URL);
-        const user = await User.findOne({ email });
-        const passwordOk = user && bcrypt.compareSync(password, user.password);
-
-        if (passwordOk) {
-          return user;
-        }
-        return null;
-      },
-    }),
-  ],
-};
+import { authOptions } from '@/app/api/authOptions'
 
 export async function isAdmin() {
   const session = await getServerSession(authOptions);
---
+++ src/app/api/authOptions.js
@@ -0,0 +1,38 @@
+export const authOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.SECRET,
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      id: "credentials",
+      credentials: {
+        username: {
+          label: "Email",
+          type: "email",
+          placeholder: "test@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const email = credentials?.email;
+        const password = credentials?.password;
+
+        mongoose.connect(process.env.MONGO_URL);
+        const user = await User.findOne({ email });
+        const passwordOk = user && bcrypt.compareSync(password, user.password);
+
+        if (passwordOk) {
+          return user;
+        }
+        return null;
+      },
+    }),
+  ],
+};

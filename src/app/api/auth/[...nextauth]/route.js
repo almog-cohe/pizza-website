ea15950dbb290cde302d0169b0b5d90836d96989
almog-cohe <almogqwerty@gmail.com>
2024-04-03 00:17:54 +0300

    Fixing file before deploy

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,52 @@
+import NextAuth, { getServerSession } from "next-auth";
+import { UserInfo } from "@/app/models/UserInfo.js";
+import { authOptions } from '@/app/api/authOptions'
 import bcrypt from "bcrypt";
 import { User } from "@/app/models/User";
 import * as mongoose from "mongoose";
-import NextAuth, { getServerSession } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "@/libs/mongoConnect.js";
-import { UserInfo } from "@/app/models/UserInfo.js";
-import { authOptions } from '@/app/api/authOptions'
+
+export const authOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.SECRET,
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      id: "credentials",
+      credentials: {
+        username: {
+          label: "Email",
+          type: "email",
+          placeholder: "test@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const email = credentials?.email;
+        const password = credentials?.password;
+
+        mongoose.connect(process.env.MONGO_URL);
+        const user = await User.findOne({ email });
+        const passwordOk = user && bcrypt.compareSync(password, user.password);
+
+        if (passwordOk) {
+          return user;
+        }
+        return null;
+      },
+    }),
+  ],
+};
 
 export async function isAdmin() {
   const session = await getServerSession(authOptions);
